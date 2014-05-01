da7c42b0ce646c8ad1bda81bdbe1f42880287cc1
ermac0 <patrickpulka@gmail.com>
2014-05-01 21:47:07 +0100

    Added copyright and license info

--- Src/FxConnectProxy.Samples/Examples/FakeBackendExample.cs
+++ Src/FxConnectProxy.Samples/Examples/FakeBackendExample.cs
@@ -1,4 +1,7 @@
-﻿using FxConnectProxy.Interfaces;
+﻿// Copyright (c) 2014 Patrick Pulka
+// License: https://raw.githubusercontent.com/ermac0/FxConnectProxy/master/LICENSE
+using FxConnectProxy.Interfaces;
+using FxConnectProxy.Validators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Src/FxConnectProxy.Samples/Examples/GetMarketDataExample.cs
+++ Src/FxConnectProxy.Samples/Examples/GetMarketDataExample.cs
@@ -1,4 +1,6 @@
-﻿using FxConnectProxy.Interfaces;
+﻿// Copyright (c) 2014 Patrick Pulka
+// License: https://raw.githubusercontent.com/ermac0/FxConnectProxy/master/LICENSE
+using FxConnectProxy.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
